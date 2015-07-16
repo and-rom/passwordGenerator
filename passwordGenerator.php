014c2e2a4c0d134aed1760f6dc0435225be88842
Andrey Romanov <andrey.a.romanov@gmail.com>
2015-07-16 20:04:02 +0300

    Add more debug variants

--- passwordGenerator.php
+++ passwordGenerator.php
@@ -1,6 +1,7 @@
 <?php
 require_once('passwordGenerator.class.php');
-$pwgen = new passwordGenerator($_GET['wordsCount'],$_GET['digitsCount'],(bool)$_GET['upperCaseLetter'],$_GET['charactersCount'],$_GET['passwordsCount']);
+if (!empty($_GET)){
+$pwgen = new passwordGenerator($_GET['wordsCount'],$_GET['digitsCount'],(bool)$_GET['upperCaseLetter'],$_GET['charactersCount'],$_GET['passwordsCount'],(isset($_GET['transliterate']) ? (bool)$_GET['transliterate'] : False));
 $pwgen->generate();
 $variant = 1;
     switch ($variant) {
@@ -12,14 +13,25 @@ $variant = 1;
       case 2:
         $pwgen->highlight("_","_");
         $pwgen->escape();
-        $pwgen->printPure();
+        $pwgen->printPreHTML();
         break;
       case 3:
+        $pwgen->highlight("_","_");
+        $pwgen->escape();
+        $pwgen->printPure();
+        break;
+      case 4:
         $pwgen->highlight("*","*");
         $pwgen->printJSON();
         break;
       default:
         $pwgen->printPure();
     }
+} else {
+$pwgen = new passwordGenerator(5,2,True,2,3,True);
+$pwgen->generate();
+$pwgen->escape();
+$pwgen->printVarDump();
+}
 exit();
 ?>
